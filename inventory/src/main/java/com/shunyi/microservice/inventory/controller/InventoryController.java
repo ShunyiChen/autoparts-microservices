ad0ea7089a382bda1595bc224a26e64675dddbb9
Simeon Chen <simeon.chen@ericsson.com>
2021-04-18 19:20:42 +0800

    fix: add a POST API

--- inventory/src/main/java/com/shunyi/microservice/inventory/controller/InventoryController.java
+++ inventory/src/main/java/com/shunyi/microservice/inventory/controller/InventoryController.java
@@ -1,16 +1,29 @@
 package com.shunyi.microservice.inventory.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSON;
+import com.shunyi.autoparts.common.entities.CommonResult;
+import com.shunyi.autoparts.common.entities.Payment;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class InventoryController {
+
+    private Map<String, CommonResult> map = new HashMap<>();
 
     /** 测试异常数 */
     @GetMapping("/testF")
     @CrossOrigin
     public String testF() {
         return "-----------test 异常数123";
+    }
+
+    @PostMapping(value = "/testF/create")
+    @CrossOrigin
+    public String create(@RequestBody Payment payment) {
+        return "插入成功";
     }
 }
